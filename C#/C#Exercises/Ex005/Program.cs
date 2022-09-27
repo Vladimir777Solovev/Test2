5ccd32d9eac4ca48714aebaab4cc2fa0c876c6b6
Your Name <you@example.com>
2022-09-27 14:32:36 +0300

    first Ex005

--- C#/C#Exercises/Ex005/Program.cs
+++ C#/C#Exercises/Ex005/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(1,10);
+Console.WriteLine(numberC);
+int result = numberA + numberB + numberC;
+
+Console.WriteLine(result);
