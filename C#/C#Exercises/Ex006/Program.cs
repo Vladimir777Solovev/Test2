026dc33aaad333ea42baa4f159e7c598f877740d
Your Name <you@example.com>
2022-09-27 15:07:54 +0300

    first Ex006

---
+++ C#/C#Exercises/Ex006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "vladimir")
+{
+    Console.WriteLine("Ave Caesar, Morituri te salutante");
+}
+else
+{
+    Console.Write("Kneel before your Emperor, ");
+    Console.Write(username);
+}
