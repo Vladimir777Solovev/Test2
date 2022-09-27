7ed6691a7b0fa827498a657e1969459f91620ca3
Your Name <you@example.com>
2022-09-27 17:30:09 +0300

    first Hw001

---
+++ C#/C#HomeWork/Hw001/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 5;
+int b = 7;
+
+int max = a;
+int min = b;
+if(a > max) max = a;
+else max = b;
+if(a < min) min = a;
+else min = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
