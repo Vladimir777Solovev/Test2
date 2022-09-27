5eae09a66932cdf270665022b1af35d0dc26da2a
Your Name <you@example.com>
2022-09-27 15:34:00 +0300

    first Ex007

---
+++ C#/C#Exercises/Ex007/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1321;
+int b = 1213;
+int c = 1323;
+int d = 1233;
+int e = 1223;
+
+int max = a;
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
