ec71dd1aa85bd358c603bc25d36b6fcda74c9e2c
Your Name <you@example.com>
2022-10-13 17:57:58 +0300

    8.1tripRework

--- C#/8.1trip/Program.cs
+++ C#/8.1trip/Program.cs
@@ -4,22 +4,8 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
-Console.WriteLine("Напишите два числа чтобы узнать какое из них большее а какое меньшее");
-int number1 = Convert.ToInt32(Console.ReadLine());
-int number2 = Convert.ToInt32(Console.ReadLine());
-int max = number1;
-int min = number2;
-
-if (number1 > max) max = number1;
-else
-{
-    max = number2;
-}
-Console.WriteLine($"Максимальное число = {max}");
-
-if (number2 < min) min = number2;
-else
-{
-    min = number1;
-}
-Console.WriteLine($"Минимальное число = {min}");
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a > b ? "Первое число больше второго" : "Второе число больше первого");
