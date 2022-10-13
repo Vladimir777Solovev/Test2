8ab05521242ce79fbb5f959d423d1de3e82674aa
Your Name <you@example.com>
2022-10-13 12:39:41 +0300

    6trip

---
+++ C#/5trip/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+while (count <= number)
+{
+    Console.Write($"{count} ");
+    count++;
+    // count = count +1;
+}
---
+++ C#/7trip/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int lastDigit = number % 10; // 456 % 10 = 6    456 / 10 = 45 / 10 = 4 / 10 = 0
+Console.WriteLine($"Последняя цифра числа = {lastDigit}");
