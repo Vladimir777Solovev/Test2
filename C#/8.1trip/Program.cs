c173e84dc4041ef523e54c2a8e20747e347ab891
Your Name <you@example.com>
2022-10-13 17:18:37 +0300

    9.1Trip

---
+++ C#/8.1trip/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.WriteLine("Напишите два числа чтобы узнать какое из них большее а какое меньшее");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+int min = number2;
+
+if (number1 > max) max = number1;
+else
+{
+    max = number2;
+}
+Console.WriteLine($"Максимальное число = {max}");
+
+if (number2 < min) min = number2;
+else
+{
+    min = number1;
+}
+Console.WriteLine($"Минимальное число = {min}");
---
+++ C#/9.1trip/Program.cs
@@ -0,0 +1,16 @@
+﻿// // Задача 4: Напишите программу, которая принимает на вход три числа
+//  и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.WriteLine("Напишите три числа, чтобы узнать какое из них меньшее");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int min = number1;
+if(number1 < min) min = number1;
+if(number2 < min) min = number2;
+if(number3 < min) min = number3;
+Console.Write("Минимальное число = ");
+Console.WriteLine(min);
