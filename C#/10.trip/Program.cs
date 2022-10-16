3543c5958d046f3db83a3ac61e7506dd949ef0e2
Your Name <you@example.com>
2022-10-16 19:05:01 +0300

    11trip

---
+++ C#/10.trip/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число является нечетным");
+}
---
+++ C#/11trip/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+while (count <= number)
+{
+Console.Write($"{count} ");
+count ++;
+}
